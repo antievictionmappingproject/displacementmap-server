958dcd00a9e721af136f62790a83d0d431f0c61b
Armine Hayrapetyan <hayrapet.armine@gmail.com>
2017-07-31 16:46:56 +0400

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ var config = {}
 
 config.db = {};
 
-config.db.user = process.env.DB_USER || 'postgres'
-config.db.pass = process.env.DB_PASS || ''
-config.db.host = process.env.DB_HOST || 'localhost'
-config.db.port = 5432
-config.db.name = process.env.DB_NAME || 'postgres'
+config.db.user = process.env.DB_USER || 'antievictionmap';
+config.db.pass = process.env.DB_PASS || 'newpassword';
+config.db.host = process.env.DB_HOST || 'aemp.cp0j5leoqqyj.us-west-1.rds.amazonaws.com';
+config.db.port = process.env.DB_PORT || 5432;
+config.db.name = process.env.DB_NAME || 'aemp';
 
 module.exports = config;
